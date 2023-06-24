e93ed52e90d700fdc1a2f384c6437e32c875cd3a
734n <ariansamouie@yahoo.co.uk>
2023-06-24 15:23:35 +0100

    add

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+const withCss = require("@zeit/next-css");
+const path = require("path");
+
+const nextConfig = {
+    webpack: (config) => {
+
+        config.resolve.alias["components"] = path.join(__dirname, "components");
+        config.resolve.alias["containers"] = path.join(__dirname, "containers");
+        return config;
+    }
+};
+
+module.exports = withPlugins(
+    [withSass, withImages, withCss],
+    nextConfig
+);
+
+module.exports = {
+    serverRuntimeConfig: {
+        PROJECT_ROOT: __dirname
+    },
+    /* Add Your Scss File Folder Path Here */
+    webpack5: false,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+}
